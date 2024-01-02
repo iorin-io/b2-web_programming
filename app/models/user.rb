d09d635f3dac5e8750d47cfaccfdd66293a509b2
iorin-io <iodere77@gmail.com>
2024-01-02 23:29:00 +0900

    balanceが負にならないように

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :balance, numericality: { greater_than_or_equal_to: 0 }
 end
