d3888b30aadafc53e200322c6bff8e843ea69803
iorin-io <iodere77@gmail.com>
2024-01-03 10:24:19 +0900

    ログアウト機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
 
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
